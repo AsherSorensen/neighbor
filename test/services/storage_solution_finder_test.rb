930eadb64f628b435fc806cbfdb3da3e0e3716b2
Asher Sorensen <mustacherasher@gmail.com>
2025-05-27 19:29:40 -0600

    add simple test case

--- test/services/storage_solution_finder_test.rb
+++ test/services/storage_solution_finder_test.rb
@@ -1,19 +1,31 @@
 require "test_helper"
 
 class StorageSolutionFinderTest < ActiveSupport::TestCase
-  setup do
-    @vehicles = [
-      { length: 10, quantity: 1 },
-      { length: 20, quantity: 2 }
-    ]
-
-    @listings = []
-  end
-
   test "finds no solutions with the empty implementation" do
-    finder = StorageSolutionFinder.new(@vehicles, @listings)
+    finder = StorageSolutionFinder.new([], [])
     solutions = finder.find_solutions
 
     assert_empty solutions
+  end
+
+  test "finds solution for single vehicle" do
+    vehicles = [{ length: 10, quantity: 1 }]
+    listings = [
+      {
+        "id" => "listing1",
+        "length" => 20,
+        "width" => 20,
+        "location_id" => "loc1",
+        "price_in_cents" => 1000
+      }
+    ]
+
+    finder = StorageSolutionFinder.new(vehicles, listings)
+    solutions = finder.find_solutions
+
+    assert_not_empty solutions
+    assert_equal "loc1", solutions.first[:location_id]
+    assert_includes solutions.first[:listing_ids], "listing1"
+    assert_equal 1000, solutions.first[:total_price_in_cents]
   end
 end 
