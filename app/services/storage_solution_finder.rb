f19b56dd39abb026eb406ea8347bc6fe00de655d
Asher Sorensen <mustacherasher@gmail.com>
2025-05-30 00:17:00 -0600

    more rubocop

--- app/services/storage_solution_finder.rb
+++ app/services/storage_solution_finder.rb
@@ -5,15 +5,15 @@ class StorageSolutionFinder
   end
 
   def find_solutions
-    listings_by_location = @listings.group_by { |listing| listing['location_id'] }
-    flat_vehicles = @vehicles.flat_map { |v| [v[:length]] * v[:quantity] }
+    listings_by_location = @listings.group_by { |listing| listing["location_id"] }
+    flat_vehicles = @vehicles.flat_map { |v| [ v[:length] ] * v[:quantity] }
     vehicle_permutations = unique_permutations(flat_vehicles)
-    
+
     results = []
-    
+
     listings_by_location.each do |location_id, location_listings|
       solution = find_cheapest_solution(location_listings, vehicle_permutations)
-      
+
       if solution
         results << {
           location_id: location_id,
@@ -22,7 +22,7 @@ class StorageSolutionFinder
         }
       end
     end
-    
+
     results.sort_by { |result| result[:total_price_in_cents] }
   end
 
@@ -33,17 +33,17 @@ class StorageSolutionFinder
 
     def self.from_listing_data(data)
       new(
-        id: data['id'],
-        length: [data['length'], data['width']].max,
-        width: [data['length'], data['width']].min,
-        price_in_cents: data['price_in_cents']
+        id: data["id"],
+        length: [ data["length"], data["width"] ].max,
+        width: [ data["length"], data["width"] ].min,
+        price_in_cents: data["price_in_cents"]
       )
     end
 
     def initialize(id:, length:, width:, price_in_cents:)
       @id = id
       @length = length
-      @used_length = [0] * (width / 10)
+      @used_length = [ 0 ] * (width / 10)
       @width = width
       @price_in_cents = price_in_cents
     end
@@ -65,29 +65,29 @@ class StorageSolutionFinder
   def find_cheapest_solution(listings, vehicle_permutations)
     best_price = Float::INFINITY
     best_listings = []
-    
+
     listing_permutations = listings.permutation.to_a
-    
+
     vehicle_permutations.each do |vehicle_permutation|
       listing_permutations.each do |listing_permutation|
         catch :invalid_solution do
             total_price = 0
             used_listings = Set.new
             listing_objs = listing_permutation.map { |l| Listing.from_listing_data(l) }
-            
+
             vehicle_permutation.each do |vehicle|
-            listing = listing_objs.find do |l|
-                next if l.price_in_cents > best_price
-                l.fit_vehicle(vehicle)
-            end
-            
-            throw :invalid_solution unless listing
-            
-            unless used_listings.include?(listing.id)
-                used_listings.add(listing.id)
-                total_price += listing.price_in_cents
-                next if total_price > best_price
-            end
+                listing = listing_objs.find do |l|
+                    next if l.price_in_cents > best_price
+                    l.fit_vehicle(vehicle)
+                end
+
+                throw :invalid_solution unless listing
+
+                unless used_listings.include?(listing.id)
+                    used_listings.add(listing.id)
+                    total_price += listing.price_in_cents
+                    next if total_price > best_price
+                end
             end
 
             if total_price < best_price
@@ -99,7 +99,7 @@ class StorageSolutionFinder
     end
 
     return nil if best_price == Float::INFINITY
-    
+
     {
       listing_ids: best_listings,
       total_price: best_price
@@ -110,17 +110,17 @@ class StorageSolutionFinder
     nums.sort!
     results = []
     used = Array.new(nums.length, false)
-  
+
     backtrack = lambda do |path|
       if path.length == nums.length
         results << path.dup
         return
       end
-  
+
       (0...nums.length).each do |i|
         next if used[i]
         next if i > 0 && nums[i] == nums[i - 1] && !used[i - 1]
-  
+
         used[i] = true
         path << nums[i]
         backtrack.call(path)
@@ -128,8 +128,8 @@ class StorageSolutionFinder
         used[i] = false
       end
     end
-  
+
     backtrack.call([])
     results
   end
-end 
+end
